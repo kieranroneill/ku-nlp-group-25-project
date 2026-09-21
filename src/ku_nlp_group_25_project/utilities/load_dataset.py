1709ad8b731523215434dcc4b89029bb1f8f9754
Kieran O'Neill <hello@kieranoneill.com>
2026-09-21 12:38:40 +0200

    chore: squash

--- src/ku_nlp_group_25_project/utilities/load_dataset.py
+++ src/ku_nlp_group_25_project/utilities/load_dataset.py
@@ -5,6 +5,12 @@ from ku_nlp_group_25_project.constants import LANGUAGES
 
 
 def load_dataset() -> tuple[DataFrame, DataFrame]:
+    """
+    Loads the TyDi XOR RC dataset and returns the training and validation dataframes.
+
+    Returns:
+        tuple[DataFrame, DataFrame]: The training and validation dataframes.
+    """
     dataset = load("coastalcph/tydi_xor_rc")
     training_data_frame = dataset["train"].to_pandas()
     validation_data_frame = dataset["validation"].to_pandas()
